f0e46f83ca7eb693ee3df7a7aec40b75c734250d
aneeshtk <aneesh.kottarath@gmail.com>
2021-08-26 09:05:44 +0530

    changes are done

--- pageObjects/LoginPage.py
+++ pageObjects/LoginPage.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 class LoginPage:
-    
+    textbox_username_id="Email"
+    textbox_password_id="Password"
+    button_login_xpath="//button[@class='button-1 login-button']"
+    link_logout_linktext="Logout"
+
     def __init__(self,driver):
         self.driver=driver
 
